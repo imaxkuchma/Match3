e37d2581b032953a041907537d9e288719c1b50c
WIN-ET4A37NKCVI\Max <imaxkuchma@gmail.com>
2024-04-10 17:24:07 +0300

    Test1

--- Assets/Match3/Scripts/Editor/Match3 Editor/View/DatabaseNavigationView.cs
+++ Assets/Match3/Scripts/Editor/Match3 Editor/View/DatabaseNavigationView.cs
@@ -19,6 +19,7 @@ namespace Match3.Match3Editor
             visualTree.CloneTree(this);
 
 
+
             firstLevelButton = this.Q<Button>("navigation__first-level-button");
             prevLevelButton = this.Q<Button>("navigation__prev-level-button");
             levelNumberField = this.Q<IntegerField>("navigation__level-number-field");
